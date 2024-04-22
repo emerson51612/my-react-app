90b634d0051ec1cc32ddbe437b4ff4c33dc0bbfa
emerson51612 <emersonrowantaylor@gmail.com>
2024-04-22 14:44:03 +1000

    hi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import LoginSignup from './components/LoginSignup/LoginSignup';
+
 
 function App() {
   return (
     <div>
       <LoginSignup/> 
-    </div>
+    </div> 
   );
 }
 
 export default App;
+
+
